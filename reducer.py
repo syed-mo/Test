89fe0b244cab6cc2c958e853cceee73f5e6cd23f
syed-mo <99827225+syed-mo@users.noreply.github.com>
2022-03-15 04:26:21 -0400

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -39,26 +39,47 @@ def main(argv):
             # count was not a number, so silently
             # ignore/discard this line
             pass
-    print(wcss)
-    tfidf = calculateTFIDF(wcss) # Implement this function
-
-def calculateTFIDF(wcss):
-    expected = '''
-    fnam1
-        word1 tfidf1
-        word2 tfidf2
-        word3 tfidf3
-          ::
-        wordn tfidfn
-    '''
+    word_map = wcss
     
-    for fnam in sorted(wcss):
-        wcs = wcss[fnam]
-        print ('\n\n', fnam)
-        sorted_wcs = dict( sorted(wcs.items(), key = lambda item: item[1], reverse = True))
-        for w in sorted_wcs:
-            print (w, sorted_wcs[w])
-    return None
+    tf_idf_details = calculate_tf_idf(word_map) # Implement this function
+    for document in sorted(tf_idf_details, key=lambda x: x[0]):
+        print(document)
+        for word in OrderedDict(sorted(tf_idf_details[document].items(), key=lambda t: t[1], reverse=True)):
+             print(word + " " + str(round(tf_idf_details[document][word], 5)))
+
+def calculate_tf(values):
+    total_words = sum(list(values.values()))
+    tf_map = {}
+    for word, occurrence in values.items():
+        tf_map[word] = occurrence/total_words
+
+    return tf_map
+
+def calculate_tf_idf(word_map):
+
+    total_documents = len(word_map)
+    idf_details = {}
+    tf_details = {}
+    tf_idf_details = {}
+
+    for document, values in word_map.items():
+        tf_idf_details[document] = {}
+        tf_details[document] = calculate_tf(values)
+        words_in_doc = list(values.keys())
+        for word in words_in_doc:
+            if word in idf_details:
+                idf_details[word] += 1
+            else:
+                idf_details[word] = 1
+
+    for word, no_of_documents in idf_details.items():
+        idf_details[word] = math.log10(total_documents/no_of_documents)
+
+    for document, values in word_map.items():
+        for word in values.keys():
+            tf_idf_details[document][word] = tf_details[document][word] * idf_details[word]
+
+    return tf_idf_details
 
 if __name__ == "__main__":
     main(sys.argv)
